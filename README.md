3c22843edb25f2ad7a27a9e821d67c76ec605cb7
monkeydunkey <bhushan.shashank93@gmail.com>
2017-03-21 19:23:11 -0700

    starting with better management system

--- README.md
+++ README.md
@@ -3,14 +3,13 @@ This is my attempt to share what I know in the field of data science with others
 
 ## Future Roadmap:
 Blogging schedule
-
-1. Week Jan 9 - Jan 15: Hypothesis Testing, Z-Statistics, T-Statistics, F-Statistics, One-Way ANOVA
-2. Week Jan 16 - Jan 22: Linear Regression
-3. Week Jan 23 - Jan 29: Random Forests
-4. Week Jan 30 - Feb 5: Gradient boosting
+1. Data Science Thought process
+2. Random Forests
+3. Hadoop setup on VMs
+4. Gradient boosting
 
 Other TODOs:
-
 1. Rearrange the resources - move images and data set to individual folders
 2. Re-structure blogs and group similar blogs
 3. Add a list of common terminology and their definitions
+4. Introduce GULP (task manager) to improve workflow
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
+
+// Set the banner content
+var banner = ['/*!\n',
+    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright 2017-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+    ' */\n',
+    ''
+].join('');
+
+
+// Copy vendor libraries from /node_modules into /vendor
+gulp.task('copy', function() {
+    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+        .pipe(gulp.dest('ThirdParty/bootstrap'))
+
+    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('ThirdParty/jquery'))
+
+    gulp.src([
+            'node_modules/font-awesome/**',
+            '!node_modules/font-awesome/**/*.map',
+            '!node_modules/font-awesome/.npmignore',
+            '!node_modules/font-awesome/*.txt',
+            '!node_modules/font-awesome/*.md',
+            '!node_modules/font-awesome/*.json'
+        ])
+        .pipe(gulp.dest('ThirdParty/font-awesome'))
+})
+
+// Run everything
+gulp.task('default', ['copy']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync'], function() {
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('js/**/*.js', browserSync.reload);
+});
