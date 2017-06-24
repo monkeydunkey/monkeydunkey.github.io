dfba57f00e41ac50e8acaada0258f6bcb3dd84a0
monkeydunkey <bhushan.shashank93@gmail.com>
2017-06-24 11:59:49 -0700

    Changing the location of the model files

--- Js/MusicGenreClassification.js
+++ Js/MusicGenreClassification.js
@@ -1,9 +1,9 @@
 //The Keras lib should be loaded before this file is called
 const model = new KerasJS.Model({
   filepaths: {
-    model: "https://github.com/monkeydunkey/monkeydunkey.github.io/tree/master/resources/Models/musicClassifier.json",
-    weights: "https://github.com/monkeydunkey/monkeydunkey.github.io/tree/master/resources/Models/resources/Models/musicClassifier.buf",
-    metadata: "https://github.com/monkeydunkey/monkeydunkey.github.io/tree/master/resources/Models/resources/Models/musicClassifier.json"
+    model: "../resources/Models/musicClassifier.json",
+    weights: "../resources/Models/musicClassifier_weights.buf",
+    metadata: "../resources/Models/musicClassifier_metadata.json"
   },
   gpu: false
 });
